da9d5bf928efe9c85f7cf91bf91bc020b3aeb923
cordie469499 <koudai06182003@gmail.com>
2025-09-19 20:55:32 +0900

    Update omikuji.js

--- omikuji.js
+++ omikuji.js
@@ -1,17 +1,15 @@
 function getRandomImage() {
   const number = Math.floor(Math.random() * 7); // 0〜6
-  return "./images/omikuji-" + number + ".png";
+  return "omikuji-" + number + ".png"; // 同じ階層にある
 }
 
 function playOmikuji() {
   const resultImg = document.querySelector("#js-result");
 
-  // 100msごとに画像を切り替えてスロット風にする
   const slotAnimationTimer = setInterval(function () {
     resultImg.setAttribute("src", getRandomImage());
   }, 100);
 
-  // 3秒後に止めて、そのときの画像を固定する
   setTimeout(function () {
     clearInterval(slotAnimationTimer);
     resultImg.setAttribute("src", getRandomImage());
